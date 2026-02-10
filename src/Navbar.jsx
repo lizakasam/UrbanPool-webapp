0b57a2171146b967c7f16d3a12c404af1c18ba25
lizakasam <lizakasam07@gmail.com>
2026-02-10 20:11:30 +0530

    initial commit

---
+++ src/Navbar.jsx
@@ -0,0 +1,28 @@
+import "./Navbar.css";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="nav-container">
+        <div className="nav-left">
+          <span className="logo">🚗</span>
+          <span className="brand">UrbanPool</span>
+        </div>
+
+        <ul className="nav-center">
+          <li className="active">Home</li>
+          <li>About</li>
+          <li>Service</li>
+          <li>Data</li>
+          <li>Clients</li>
+          <li>Contact</li>
+        </ul>
+
+        <div className="nav-right">
+          <span className="login">Log In</span>
+          <button className="signup">Sign Up</button>
+        </div>
+      </div>
+    </nav>
+  );
+}
